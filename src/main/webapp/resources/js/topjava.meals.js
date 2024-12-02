cf2f242dd02eedbdb82b9505ab0ef694bf49771e
Kvitto <kian04@mail.ru>
2024-12-03 00:57:32 +0300

    HW08_1_AJAX_meals_MealUIController

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,53 @@
+﻿const mealAjaxUrl = "profile/meals/";
+
+// https://stackoverflow.com/a/5064235/548473
+const ctx = {
+    ajaxUrl: mealAjaxUrl
+};
+
+// $(document).ready(function () {
+$(function () {
+    makeEditable(
+        $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "asc"
+                ]
+            ]
+        })
+    );
+});
+
+function clearFilter() {
+    $("#filter")[0].reset();
+    $.get(ctx.ajaxUrl, function (data) {
+        ctx.datatableApi.clear().rows.add(data).draw();
+    });
+}
+
+// function filtered() {
+//     form.find(":input").val("");
+//     $("#filter").modal();
+// }
