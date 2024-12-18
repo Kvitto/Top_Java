e8cf748640446cabe387be5e69095ecd374ea7d3
Kvitto <kian04@mail.ru>
2024-12-19 01:01:57 +0300

    10_12_controller_advice_exception

---
+++ src/main/java/ru/javawebinar/topjava/util/exception/ErrorType.java
@@ -0,0 +1,8 @@
+﻿package ru.javawebinar.topjava.util.exception;
+
+public enum ErrorType {
+    APP_ERROR,
+    DATA_NOT_FOUND,
+    DATA_ERROR,
+    VALIDATION_ERROR
+}
---
+++ src/main/java/ru/javawebinar/topjava/util/exception/IllegalRequestDataException.java
@@ -0,0 +1,7 @@
+﻿package ru.javawebinar.topjava.util.exception;
+
+public class IllegalRequestDataException extends RuntimeException {
+    public IllegalRequestDataException(String msg) {
+        super(msg);
+    }
+}
